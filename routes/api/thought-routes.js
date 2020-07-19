3c9c03dd10196e9006c46f4119e221a3b4ebaa68
Thor40 <nicholas.kosik@gmail.com>
2020-07-19 10:58:26 -0500

    thought-routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { addThought,
+        removeThought,
+        addReaction,
+        removeReaction
+    } = require('../../controllers/thought-controller');
+
+// /api/thoughts/<userId>
+router.route('/:userId').post(addThought);
+
+// /api/thoughts/<userId>/<thoughtId> (2 params, after deleteing comment, which user that thought originated from)
+router.route('/:userId/:thoughtId')
+    .put(addReaction)
+    .delete(removeThought);
+
+// /api/thoughts/<userId>/<thoughtId>/<reactionId> (3 params, userId, thoughtId, reactionId to delete the corresponding reaction)
+reouter.route(':userId/:thoughtId/:reactionId').delete(removeReaction);
+
+module.exports = router;
