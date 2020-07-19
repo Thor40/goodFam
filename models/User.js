b169fb97135f5ed3a58f3c419819d3feb12ee717
Thor40 <nicholas.kosik@gmail.com>
2020-07-19 16:08:07 -0500

    model/controller/route updated

--- models/User.js
+++ models/User.js
@@ -10,23 +10,23 @@ const UserSchema = new Schema ({
         },
     email: {
         type: String,
-        required: [true, "Email required"],
+        required: [true, "Email required!"],
         unique: true,
         validate: {
             validator: function(isEmail) {
                 return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(isEmail);
             },
             message: 'Please enter a valid email'
-        },
+            }
         },
     thoughts: {
         type: Schema.Types.ObjectId,
         ref: 'Thought'
         },
-    // friends: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'User'
-    //     }
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+        }]
     },
     {
         toJSON: {
@@ -39,9 +39,9 @@ const UserSchema = new Schema ({
 );
 
 // virtual that keeps count of the friends
-// UserSchema.virtual('friendCount').get(function() {
-//     return friends.reduce((total, friends) => total + friends.length + 1, 0);
-// });
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.reduce((total, friends) => total + friends.length + 1, 0);
+});
 
 // create user model
 const User = model('User', UserSchema);
